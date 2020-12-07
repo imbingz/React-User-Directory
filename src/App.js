53ad7fa4b8cee1249e43be39a898fd52d3c48283
Bing Z <bingz@me.com>
2020-12-06 23:57:55 -0700

    changed data to dataValue on sortiing func

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import SortBy from './components/SortBy';
 import Table from './components/Table';
 
 function App() {
-  //Search Bar Component 
+  //set state hook 
   const [ inputValue, setInputValue ] = useState('');
   const [dataValue, setDataValue] = useState(data)
+  const [sortValue, setSortValue] = useState('')
 
+  //Handle search input
 	const handleInputChange = e => {
     setDataValue(data)
     setInputValue(e.target.value);
-	};
+  };
+  
+  //handle find button click 
 	const handleClick = e => {
     e.preventDefault();
     if(inputValue) {
@@ -26,56 +30,55 @@ function App() {
   };
   
   //SortBy Component 
-  const [sortValue, setSortValue] = useState('')
   const handleSortValueChange = (e) => {
     setSortValue(e.target.value);
   }
 
   //Sorting funcationality
   if(sortValue === "First Name:(A-Z)") {
-    data.sort((a, b) => {
+    dataValue.sort((a, b) => {
       if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
       if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
       return 0;
     });
   } else if(sortValue === "First Name:(Z-A)") {
-    data.sort((a, b) => {
+    dataValue.sort((a, b) => {
       if (a.name.toLowerCase() > b.name.toLowerCase()) return -1;
       if (a.name.toLowerCase() < b.name.toLowerCase()) return 1;
       return 0;
     });
   } else if(sortValue === "Title:(A-Z)") {
-    data.sort((a, b) => {
+    dataValue.sort((a, b) => {
       if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
       if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
       return 0;
     });
   } else if (sortValue === "Title:(Z-A)") {
-    data.sort((a, b) => {
+    dataValue.sort((a, b) => {
       if (a.title.toLowerCase() > b.title.toLowerCase()) return -1;
       if (a.title.toLowerCase() < b.title.toLowerCase()) return 1;
       return 0;
     });
   } else if(sortValue === "Location:(A-Z)") {
-    data.sort((a, b) => {
+    dataValue.sort((a, b) => {
       if (a.location.toLowerCase() > b.location.toLowerCase()) return 1;
       if (a.location.toLowerCase() < b.location.toLowerCase()) return -1;
       return 0;
     });
   } else if (sortValue === "Location:(Z-A)") {
-    data.sort((a, b) => {
+    dataValue.sort((a, b) => {
       if (a.location.toLowerCase() > b.location.toLowerCase()) return -1;
       if (a.location.toLowerCase() < b.location.toLowerCase()) return 1;
       return 0;
     });
   } else if(sortValue === "Availability:(A-Z)") {
-    data.sort((a, b) => {
+    dataValue.sort((a, b) => {
       if (a.isActive > b.isActive) return -1;
       if (a.isActive < b.isActive) return 1;
       return 0;
     });
   } else {
-    data.sort((a, b) => {
+    dataValue.sort((a, b) => {
       if (a.isActive > b.isActive) return 1;
       if (a.isActive < b.isActive) return -1;
       return 0;
