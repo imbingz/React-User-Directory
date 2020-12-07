e776683d8b6b244e9546ed6236e6698dcc9c6abd
Bing Z <bingz@me.com>
2020-12-06 20:09:08 -0700

    added  employee mock data.js

---
+++ src/data.js
@@ -0,0 +1,162 @@
+export const data = [
+	{
+		_id: '5fcb07c9208da39dbc01bc3b',
+		title: 'Operation Manager',
+		isActive: false,
+		picture: {
+			large: 'https://randomuser.me/api/portraits/women/14.jpg',
+			thumbnail: 'https://randomuser.me/api/portraits/thumb/women/14.jpg'
+		},
+		age: 51,
+		eyeColor: 'green',
+		name: 'Karin Fisher',
+		gender: 'female',
+		email: 'karinfisher@musanpoly.com',
+		phone: '+1 (968) 517-3499',
+		location: 'Arizona',
+		address: '927 Oriental Court, Jenkinsville, Arizona, 1937',
+		about:
+			'Quis anim elit nostrud esse do nulla est in laboris in labore laborum in adipisicing. Tempor ex cupidatat ea Lorem esse fugiat velit cillum culpa Lorem. Adipisicing sint nisi duis sit est ut incididunt esse consectetur Lorem do cillum. Eiusmod exercitation irure aute incididunt in ea exercitation sunt enim deserunt in. Aliqua ex aliqua consequat duis. Voluptate mollit nostrud anim ad occaecat dolor in ipsum velit amet laborum deserunt aliqua eu.\r\n',
+		registered: '2015-01-08T05:06:25 +07:00'
+	},
+	{
+		_id: '5fcb07c99979f607bfe2ccc8',
+		title: 'UX Designer',
+		isActive: true,
+		picture: {
+			large: 'https://randomuser.me/api/portraits/men/14.jpg',
+			thumbnail: 'https://randomuser.me/api/portraits/thumb/men/14.jpg'
+		},
+		age: 45,
+		eyeColor: 'blue',
+		name: 'Cohen Gibson',
+		gender: 'male',
+		email: 'cohengibson@musanpoly.com',
+		phone: '+1 (949) 587-2288',
+		location: 'Louisiana',
+		address: '519 Fleet Place, Martinsville, Louisiana, 200',
+		about:
+			'Dolor ut sit velit eu aliquip sint sunt duis incididunt. Enim veniam incididunt anim est exercitation fugiat duis elit minim voluptate in occaecat nulla ad. Qui tempor incididunt sint ad pariatur minim exercitation est commodo Lorem irure sit. Ea culpa eu ex quis dolore esse fugiat ut est amet ut. Do ex incididunt officia enim enim adipisicing irure. Quis adipisicing laborum in occaecat do irure officia.\r\n',
+		registered: '2014-10-30T01:04:31 +06:00'
+	},
+	{
+		_id: '5fcb07c914a3a1606f9a7ee2',
+		title: 'Senior Developer',
+		isActive: true,
+		picture: {
+			large: 'https://randomuser.me/api/portraits/men/1.jpg',
+			thumbnail: 'https://randomuser.me/api/portraits/thumb/men/1.jpg'
+		},
+		age: 42,
+		eyeColor: 'green',
+		name: 'Pitts Clarke',
+		gender: 'male',
+		email: 'pittsclarke@musanpoly.com',
+		phone: '+1 (821) 555-3307',
+		location: 'Indiana',
+		address: '389 Veronica Place, Sanford, Indiana, 2110',
+		about:
+			'Commodo cillum Lorem incididunt sint duis labore reprehenderit enim. Ea pariatur anim aliquip aute labore velit exercitation sunt. Sunt tempor esse duis nostrud sint cillum consequat nostrud. Eu laboris officia non id amet aute nulla sint occaecat consequat exercitation aliqua. Dolor et tempor velit aliquip Lorem eu adipisicing do eu enim. Proident nulla minim consequat culpa exercitation veniam.\r\n',
+		registered: '2020-12-04T05:13:03 +07:00'
+	},
+	{
+		_id: '5fcb07c9a6e091f07c94785a',
+		title: 'HR Manager',
+		isActive: false,
+		picture: {
+			large: 'https://randomuser.me/api/portraits/men/4.jpg',
+			thumbnail: 'https://randomuser.me/api/portraits/thumb/men/4.jpg'
+		},
+		age: 39,
+		eyeColor: 'green',
+		name: 'Frazier Davidson',
+		gender: 'male',
+		email: 'frazierdavidson@musanpoly.com',
+		phone: '+1 (832) 449-3242',
+		location: 'Michigan',
+		address: '962 Sandford Street, Roulette, Michigan, 1618',
+		about:
+			'Eu voluptate deserunt sit nostrud nisi ullamco quis sunt ipsum ex ut est ullamco. Adipisicing adipisicing nulla sint esse voluptate quis labore pariatur sit excepteur minim. Proident minim tempor cupidatat deserunt tempor ut deserunt elit incididunt velit. Laborum labore adipisicing dolore mollit. Consectetur laboris dolor officia cupidatat amet duis consequat anim ex nostrud in dolore sint.\r\n',
+		registered: '2017-10-29T06:13:52 +06:00'
+	},
+	{
+		_id: '5fcb07c9ab3ebd27096a8bc7',
+		title: 'Risk Controller',
+		isActive: true,
+		picture: {
+			large: 'https://randomuser.me/api/portraits/women/1.jpg',
+			thumbnail: 'https://randomuser.me/api/portraits/thumb/women/1.jpg'
+		},
+		age: 38,
+		eyeColor: 'green',
+		name: 'Alisha Trevino',
+		gender: 'female',
+		email: 'alishatrevino@musanpoly.com',
+		phone: '+1 (879) 484-2842',
+		location: 'Washington',
+		address: '806 Denton Place, Topaz, Washington, 4877',
+		about:
+			'Labore consequat cillum sunt et est occaecat velit in laborum laboris deserunt incididunt. Proident non ullamco fugiat ea et. Occaecat elit mollit officia cupidatat eiusmod nisi amet amet. Exercitation anim pariatur mollit ex consectetur mollit incididunt. Qui consequat dolor enim irure est tempor dolore ex. Reprehenderit nulla dolor consectetur ullamco pariatur sint.\r\n',
+		registered: '2017-03-21T02:38:40 +06:00'
+	},
+	{
+		_id: '5fcb07c903b6ef5e87df144f',
+		title: 'Creative Director',
+		isActive: false,
+		picture: {
+			large: 'https://randomuser.me/api/portraits/women/4.jpg',
+			thumbnail: 'https://randomuser.me/api/portraits/thumb/women/4.jpg'
+		},
+		age: 28,
+		eyeColor: 'blue',
+		name: 'Willie Hester',
+		gender: 'female',
+		email: 'williehester@musanpoly.com',
+		phone: '+1 (866) 466-2634',
+		location: 'Virgin Islands',
+		address: '669 Irvington Place, Dawn, Virgin Islands, 7647',
+		about:
+			'Dolor ipsum aute officia culpa ut officia et ullamco velit. Esse Lorem elit irure sunt veniam nisi irure commodo nostrud commodo dolor dolor commodo. Qui eiusmod nulla nulla qui pariatur nisi. Exercitation magna aute quis enim ea exercitation ipsum aliqua adipisicing commodo. Commodo amet occaecat aliquip ad consectetur incididunt dolore id.\r\n',
+		registered: '2018-03-22T09:42:13 +06:00'
+	},
+	{
+		_id: '5fcb07c95f02adc74680cfef',
+		title: 'Project Manager',
+		isActive: false,
+		picture: {
+			large: 'https://randomuser.me/api/portraits/women/2.jpg',
+			thumbnail: 'https://randomuser.me/api/portraits/thumb/women/2.jpg'
+		},
+		age: 33,
+		eyeColor: 'brown',
+		name: 'Myrtle Barnett',
+		gender: 'female',
+		email: 'myrtlebarnett@musanpoly.com',
+		phone: '+1 (888) 537-2442',
+		location: 'Georgia',
+		address: '244 Columbia Street, Thatcher, Georgia, 7330',
+		about:
+			'Velit velit elit velit in qui. Magna ipsum nisi aute velit velit sint aute aliquip aliqua exercitation. Nostrud magna voluptate minim Lorem eiusmod eu. Ullamco ea mollit esse ut mollit pariatur aliquip commodo ut quis laboris eu excepteur. Et nostrud proident ea esse reprehenderit nulla.\r\n',
+		registered: '2020-04-16T08:09:32 +06:00'
+	},
+	{
+		_id: '5fcb07c9b35ce6c3cf2031cb',
+		title: 'Accountant',
+		isActive: true,
+		picture: {
+			large: 'https://randomuser.me/api/portraits/women/3.jpg',
+			thumbnail: 'https://randomuser.me/api/portraits/thumb/women/3.jpg'
+		},
+		age: 32,
+		eyeColor: 'green',
+		name: 'Jennifer Johns',
+		gender: 'female',
+		email: 'jenniferjohns@musanpoly.com',
+		phone: '+1 (828) 457-3624',
+		location: 'Vermont',
+		address: '496 Whitty Lane, Valle, Vermont, 7732',
+		about:
+			'Mollit in enim fugiat excepteur elit officia. In occaecat est voluptate non ea officia sit ad. Non ullamco aliqua duis officia Lorem commodo ad incididunt minim. Lorem eiusmod veniam ipsum voluptate. Commodo aute quis adipisicing exercitation pariatur nulla ex. Aliqua ex laborum aliqua ea tempor. Duis deserunt labore aute adipisicing eiusmod culpa duis.\r\n',
+		registered: '2017-12-31T07:20:08 +07:00'
+	}
+];
