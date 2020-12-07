a1289c23ad156183e4c701fdd5616cbc9b315ce3
Bing Z <bingz@me.com>
2020-12-06 19:55:18 -0700

    initial components dir setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
+import moduleName from './components/Heading';
 import './App.css';
+import Heading from './components/Heading';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Heading />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Heading/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Heading() {
+	return (
+		<header>
+			<h1>Employee Directory</h1>
+		</header>
+	);
+}
+
+export default Heading;
