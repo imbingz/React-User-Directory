4f7ad85dfee5677b6bfbd1a2135efbd6beca8316
BING <bingz@me.com>
2020-12-07 21:26:56 -0700

    Update TableBody.js

--- src/components/TableBody/TableBody.js
+++ src/components/TableBody/TableBody.js
@@ -6,29 +6,29 @@ function TableBody({ picture, name, title, location, isActive, phone, email, abo
 
 	return (
 		<>
-			<tr className="tbodyRow">
-				<td>
-               {picture.thumbnail? <img src={picture.thumbnail} alt={name}/> : <img src='http://placehold.it/50x50' alt='placeholder'/>}
-					
-				</td>
-				<td>{name}</td>
-				<td>{title}</td>
-				<td>{location}</td>
-				<td>{isActive ? 'Available' : 'Out of Office'}</td>
-				<td>
-					<button onClick={() => setIsHidden(!isHidden)} type='submit'>
-						{isHidden ? 'more' : 'less'}
-					</button>
-				</td>
-			</tr>
-         {!isHidden && (
-            <>
-               <tr className="moreInfo"><td><strong>Phone: </strong> {phone}</td></tr>
-               <tr className="moreInfo"><td><strong>Email: </strong> {email}</td></tr>
-               <tr className="moreInfo"><td><strong>About: </strong> {about}</td></tr> 
-            </>
-         )}
-		</>
+		   <tr className="tbodyRow">
+			<td>
+		       {picture.thumbnail? <img src={picture.thumbnail} alt={name}/> : <img src='http://placehold.it/50x50' alt='placeholder'/>}
+
+			</td>
+			<td>{name}</td>
+			<td>{title}</td>
+			<td>{location}</td>
+			<td>{isActive ? 'Available' : 'Out of Office'}</td>
+			<td>
+			<button onClick={() => setIsHidden(!isHidden)} type='submit'>
+			{isHidden ? 'more' : 'less'}
+			</button>
+			</td>
+		  </tr>
+		 {!isHidden && (
+		    <>
+		       <tr className="moreInfo"><td><strong>Phone: </strong> {phone}</td></tr>
+		       <tr className="moreInfo"><td><strong>Email: </strong> {email}</td></tr>
+		       <tr className="moreInfo"><td><strong>About: </strong> {about}</td></tr> 
+		    </>
+		 )}
+	     </>
 	);
 }
 
