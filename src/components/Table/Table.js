439b08b41ab909cf8031bd4df19a19dc4692b04a
Bing Z <bingz@me.com>
2020-12-08 13:33:43 -0700

    render table header dynamically

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,10 +4,20 @@ import TableBody from '../TableBody';
 import './Table.css';
 
 function Table({ data }) {
+	//set table column header's contents
+	const cols = [
+		{ colName: '' },
+		{ colName: 'Name' },
+		{ colName: 'Title' },
+		{ colName: 'Location' },
+		{ colName: 'Availability' },
+		{ colName: '' }
+	];
+
 	return (
 		<section className='employees'>
 			<table>
-				<TableHead />
+				<TableHead cols={cols} />
 				<tbody>
 					{data.map(person => {
 						return <TableBody key={person._id} {...person} data={data} />;
--- src/components/TableHead/TableHead.js
+++ src/components/TableHead/TableHead.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import './TableHead.css';
 
-function TableHead() {
+function TableHead({ cols }) {
 	return (
 		<thead>
-			<tr className='tableHeader'>
-				<th />
-				<th>Name</th>
-				<th>Title</th>
-				<th>Location</th>
-				<th>Availability</th>
-				<th />
-			</tr>
+			<tr className='tableHeader'>{cols.map((col, i) => <th key={i}>{col.colName}</th>)}</tr>
 		</thead>
 	);
 }
